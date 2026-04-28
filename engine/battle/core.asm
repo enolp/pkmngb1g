3482fe27f91033e62f4c037b073aed6a4c65c5a8
enolp <enolp@softastur.org>
2026-04-29 00:57:57 +0200

    Modificación de la posición del cursor nel diálogu de llucha

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -2066,9 +2066,9 @@ DisplayBattleMenu::
 	ld a, ' '
 	jr z, .safariLeftColumn
 ; put cursor in left column for normal battle menu (i.e. when it's not a Safari battle)
-	ldcoord_a 15, 14 ; clear upper cursor position in right column
-	ldcoord_a 15, 16 ; clear lower cursor position in right column
-	ld b, $9 ; top menu item X
+	ldcoord_a 11, 14 ; clear upper cursor position in right column
+	ldcoord_a 11, 16 ; clear lower cursor position in right column
+	ld b, $1 ; top menu item X
 	jr .leftColumn_WaitForInput
 .safariLeftColumn
 	ldcoord_a 13, 14
@@ -2099,9 +2099,9 @@ DisplayBattleMenu::
 	ld a, ' '
 	jr z, .safariRightColumn
 ; put cursor in right column for normal battle menu (i.e. when it's not a Safari battle)
-	ldcoord_a 9, 14 ; clear upper cursor position in left column
-	ldcoord_a 9, 16 ; clear lower cursor position in left column
-	ld b, $f ; top menu item X
+	ldcoord_a 1, 14 ; clear upper cursor position in left column
+	ldcoord_a 1, 16 ; clear lower cursor position in left column
+	ld b, $b ; top menu item X
 	jr .rightColumn_WaitForInput
 .safariRightColumn
 	ldcoord_a 1, 14 ; clear upper cursor position in left column
